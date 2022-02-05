cc6549b397a07b47644867c377a52cf1b88f6c6e
Taras <yand3x.ambassador@yandex.ru>
2022-02-05 22:55:13 +0500

    Карту можно двигать(реализованы все события клавиш клавиатуры)

--- mapapi.py
+++ mapapi.py
@@ -4,7 +4,7 @@ from ui import Ui_MapQT
 import pygame
 import requests
 import os
-from  PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox
 
 
 class Window(Ui_MapQT, QMainWindow):
@@ -12,75 +12,115 @@ class Window(Ui_MapQT, QMainWindow):
         super().__init__()
         self.setupUi(self)
         self.button_find_maoa.clicked.connect(self.letsgo)
+        self.Gibrid.clicked.connect(self.gibr)
+        self.Sputnik.clicked.connect(self.sputni)
+        self.Sxema.clicked.connect(self.shema)
 
     def letsgo(self):
         try:
-            self.shir = int(self.lineEdit_S.text())
-            self.dolg = int(self.lineEdit_D.text())
-            self.scale = int(self.scale.text())
+            self.shir = float(self.spin_S.value())
+            self.dolg = float(self.spin_D.value())
+            self.z = int(self.scale.value())
         except Exception:
-            self.ERROR_WINDOW('Неправильный формат данных')
+            ERROR_WINDOW('Неправильный формат данных')
+            return
         self.gib = self.Gibrid.isChecked()
         self.spu = self.Sputnik.isChecked()
         post = self.checkBox.isChecked()
         place = self.place.text()
-        try:
-            if self.gib:
-                ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={self.shir},{self.dolg}&z={self.scale}&l=sat,skl"
-            elif self.spu:
-                ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={self.shir},{self.dolg}&z={self.scale}&l=sat"
-            else:
-                ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={self.shir},{self.dolg}&z={self.scale}&l=map"
-            response = requests.get(ya_statick)
-            map_file = "map.png"
-            with open(map_file, "wb") as file:
-                file.write(response.content)
-            pygame.init()
-            self.screen = pygame.display.set_mode((600, 450))
-            self.screen.blit(pygame.image.load(map_file), (0, 0))
-        except Exception:
-            self.ERROR_WINDOW('Не удалось найти карту')
-            pygame.quit()
-            return
-        pygame.display.flip()
-        while pygame.event.wait().type != pygame.QUIT:
-            self.pg_update()
+        ya_map = PGMap(self.shir, self.dolg, self.z)
+
+    def gibr(self):
+        self.gib = self.Gibrid.isChecked()
+        print(self.gib)
+
+    def sputni(self):
+        self.spu = self.Sputnik.isChecked()
+        print(self.spu)
+
+    def shema(self):
+        self.gib = self.spu = False
+
+
+class PGMap:
+    def __init__(self, SH, DL, z):
+        self.shirina = SH
+        self.dolgota = DL
+        self.scale = z
+        if window.gib:
+            ya_statick =\
+                f"https://static-maps.yandex.ru/1.x/?ll={self.shirina},{self.dolgota}&z={self.scale}&l=sat,skl"
+        elif window.spu:
+            ya_statick =\
+                f"https://static-maps.yandex.ru/1.x/?ll={self.shirina},{self.dolgota}&z={self.scale}&l=sat"
+        else:
+            ya_statick =\
+                f"https://static-maps.yandex.ru/1.x/?ll={self.shirina},{self.dolgota}&z={self.scale}&l=map"
+        response = requests.get(ya_statick)
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        pygame.init()
+        self.screen = pygame.display.set_mode((600, 450))
+        self.screen.blit(pygame.image.load(map_file), (0, 0))
+        self.running = True
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                key = pygame.key.get_pressed()
+                if key[pygame.K_UP]:
+                    if self.dolgota + 5 / self.scale ** 2 <= 90:
+                        self.dolgota += 5 / self.scale ** 2
+                        self.zamena()
+                elif key[pygame.K_DOWN]:
+                    if self.dolgota - 5 / self.scale ** 2 >= -90:
+                        self.dolgota -= 5 / self.scale ** 2
+                        self.zamena()
+                elif key[pygame.K_LEFT]:
+                    if self.shirina - 5 / self.scale ** 2 >= -180:
+                        self.shirina -= 5 / self.scale ** 2
+                        self.zamena()
+                elif key[pygame.K_RIGHT]:
+                    if self.shirina + 5 / self.scale ** 2 <= 180:
+                        self.shirina += 5 / self.scale ** 2
+                        self.zamena()
+                elif key[pygame.K_PAGEDOWN]:
+                    if self.scale - 1 >= 1:
+                        self.scale -= 1
+                        self.zamena()
+                elif key[pygame.K_PAGEUP]:
+                    if self.scale + 1 <= 17:
+                        self.scale += 1
+                        self.zamena()
+            pygame.display.flip()
         pygame.quit()
         os.remove(map_file)
 
-    def pg_update(self):
-        key = pygame.key.get_pressed()
-        if key[pygame.K_UP]:
-            self.dolg += 5
-            self.lineEdit_D.setText(str(self.dolg))
-        if key[pygame.K_DOWN]:
-            self.dolg -= 5
-            self.lineEdit_D.setText(str(self.dolg))
-        if key[pygame.K_LEFT]:
-            self.shir += 5
-            self.lineEdit_S.setText(str(self.shir))
-        if key[pygame.K_RIGHT]:
-            self.shir -= 5
-            self.lineEdit_S.setText(str(self.shir))
-        if self.gib:
-            ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={self.shir},{self.dolg}&z={self.scale}&l=sat,skl"
-        elif self.spu:
-            ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={self.shir},{self.dolg}&z={self.scale}&l=sat"
+    def zamena(self):
+        if window.gib:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={self.shirina},{self.dolgota}&z={self.scale}&l=sat,skl"
+        elif window.spu:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={self.shirina},{self.dolgota}&z={self.scale}&l=sat"
         else:
-            ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={self.shir},{self.dolg}&z={self.scale}&l=map"
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={self.shirina},{self.dolgota}&z={self.scale}&l=map"
         response = requests.get(ya_statick)
         map_file = "map.png"
         with open(map_file, "wb") as file:
             file.write(response.content)
         self.screen.blit(pygame.image.load(map_file), (0, 0))
 
-    def ERROR_WINDOW(self, text):
-        ERROR = QMessageBox()
-        ERROR.setWindowTitle('Упс, что-то пошло не так... 🤔')
-        ERROR.setText(text)
-        ERROR.setIcon(QMessageBox.Warning)
-        ERROR.setStandardButtons(QMessageBox.Cancel)
-        ERROR.exec_()
+
+def ERROR_WINDOW(text):
+    ERROR = QMessageBox()
+    ERROR.setWindowTitle('Упс, что-то пошло не так... 🤔')
+    ERROR.setText(text)
+    ERROR.setIcon(QMessageBox.Warning)
+    ERROR.setStandardButtons(QMessageBox.Cancel)
+    ERROR.exec_()
 
 
 def except_hook(cls, exception, traceback):
