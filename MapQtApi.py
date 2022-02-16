e12784f49b82b1571ae8530e08ddf75f7090c7e3
Dark_Angel2049 <chichinvenu@gmail.com>
2022-02-17 01:12:24 +0500

    Работают метки

--- MapQtApi.py
+++ MapQtApi.py
@@ -21,24 +21,37 @@ class Main_Window(QMainWindow):
         self.spn = [0.3, 0.3]
         self.coordinates = [37.617644, 55.755819]
         self.map_type = []
+        self.mark_on = False
         self.shema_on()
 
-    def gibrid_on(self):
-        self.map_type.append("gibrid_on")
-        ya_statick = \
-            f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat,skl&size=650,450"
+    def sputnik_on(self):
+        self.map_type.append("sputnik_on")
+        if self.mark_on:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat,skl&size=650,450&{self.point_param}"
+        else:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat,skl&size=650,450"
         self.map_image(ya_statick)
 
     def sputnik_on(self):
         self.map_type.append("sputnik_on")
-        ya_statick = \
-            f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat&size=650,450"
+        if self.mark_on:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat&size=650,450&{self.point_param}"
+        else:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat&size=650,450"
         self.map_image(ya_statick)
 
     def shema_on(self):
         self.map_type.append("shema_on")
-        ya_statick = \
-            f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=map&size=650,450"
+        if self.mark_on:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=map&size=650,450&{self.point_param}"
+        else:
+            ya_statick = \
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=map&size=650,450"
         self.map_image(ya_statick)
 
     def map_image(self, ya_statick):
@@ -52,13 +65,26 @@ class Main_Window(QMainWindow):
 
     def mark_image(self):
         try:
+            self.mark_on = True
             self.toponym_to_find = self.place.text()
-            map_type = "sat"
+            if self.map_type[-1] == "gibrid_on":
+                map_type = "sat,skl"
+            elif self.map_type[-1] == "shema_on":
+                map_type = "map"
+            else:
+                map_type = "sat"
             ll, spn = get_ll_span(self.toponym_to_find)
             ll_spn = f"ll={ll}&spn={spn}"
-            point_param = f"pt={ll}"
+            self.point_param = f"pt={ll}"
+            self.spn = spn
+            self.spn = spn.split(",")
+            self.spn[0] = float(self.spn[0])
+            self.spn[1] = float(self.spn[1])
+            self.coordinates = ll.split(",")
+            self.coordinates[0] = float(self.coordinates[0])
+            self.coordinates[1] = float(self.coordinates[1])
             map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}"
-            map_request += "&" + point_param
+            map_request += "&" + self.point_param
             response = requests.get(map_request)
             if not response:
                 print("Ошибка выполнения запроса:")
