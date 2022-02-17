b492c3e77dcb99422f1036f0ebbd31627702da9b
Taras <yand3x.ambassador@yandex.ru>
2022-02-18 02:06:35 +0500

    Метка на ЛКМ (№11) и куча других изменений

--- MapQtApi.py
+++ MapQtApi.py
@@ -21,38 +21,28 @@ class Main_Window(QMainWindow):
         self.postal_code_button.clicked.connect(self.postal_code_click)
         self.spn = [0.3, 0.3]
         self.coordinates = [37.617644, 55.755819]
-        self.map_type = []
+        self.map_type = ['map']
         self.mark_on = False
+        self.point_param = ''
         self.shema_on()
+        self.ll_spn = f"ll={self.coordinates[0]}"
+        self.ll_spn += f",{self.coordinates[1]}"
+        self.ll_spn += f"&spn={self.spn[0]}"
+        self.ll_spn += f",{self.spn[1]}"
 
     def sputnik_on(self):
-        self.map_type.append("sputnik_on")
-        if self.mark_on:
-            ya_statick = \
-                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat,skl&size=650,450&{self.point_param}"
-        else:
-            ya_statick = \
-                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat,skl&size=650,450"
+        self.map_type.append("sat")
+        ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat&size=650,450&{self.point_param}"
         self.map_image(ya_statick)
 
     def gibrid_on(self):
-        self.map_type.append("sputnik_on")
-        if self.mark_on:
-            ya_statick = \
-                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat&size=650,450&{self.point_param}"
-        else:
-            ya_statick = \
-                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat&size=650,450"
+        self.map_type.append("sat,skl")
+        ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=sat,skl&size=650,450&{self.point_param}"
         self.map_image(ya_statick)
 
     def shema_on(self):
-        self.map_type.append("shema_on")
-        if self.mark_on:
-            ya_statick = \
-                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=map&size=650,450&{self.point_param}"
-        else:
-            ya_statick = \
-                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=map&size=650,450"
+        self.map_type.append("map")
+        ya_statick = f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l=map&size=650,450&{self.point_param}"
         self.map_image(ya_statick)
 
     def map_image(self, ya_statick):
@@ -65,82 +55,123 @@ class Main_Window(QMainWindow):
         os.remove(map_file)
 
     def mark_image(self):
+        self.mark_on = True
+        self.toponym_to_find = self.place.text()
+        ll, spn = get_ll_span(self.toponym_to_find)
+        self.ll_spn = f"ll={ll}&spn={spn}"
+        self.point_param = f"pt={ll}"
+        self.spn = spn
+        self.spn = spn.split(",")
+        self.spn[0] = float(self.spn[0])
+        self.spn[1] = float(self.spn[1])
+        self.coordinates = ll.split(",")
+        self.coordinates[0] = float(self.coordinates[0])
+        self.coordinates[1] = float(self.coordinates[1])
+        self.map_request = f"http://static-maps.yandex.ru/1.x/?{self.ll_spn}&l={self.map_type[-1]}&&size=650,450"
+        self.map_request += "&" + self.point_param
+        response = requests.get(self.map_request)
+        map_file = 'map.png'
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        self.adress = get_nearest_object(self.coordinates, kind="house")
         try:
-            self.mark_on = True
-            self.toponym_to_find = self.place.text()
-            if self.map_type[-1] == "gibrid_on":
-                map_type = "sat,skl"
-            elif self.map_type[-1] == "shema_on":
-                map_type = "map"
-            else:
-                map_type = "sat"
-            ll, spn = get_ll_span(self.toponym_to_find)
-            ll_spn = f"ll={ll}&spn={spn}"
-            self.point_param = f"pt={ll}"
-            self.spn = spn
-            self.spn = spn.split(",")
-            self.spn[0] = float(self.spn[0])
-            self.spn[1] = float(self.spn[1])
-            self.coordinates = ll.split(",")
-            self.coordinates[0] = float(self.coordinates[0])
-            self.coordinates[1] = float(self.coordinates[1])
-            map_request = f"http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}&&size=650,450"
-            map_request += "&" + self.point_param
-            response = requests.get(map_request)
-            map_file = 'map.png'
-            with open(map_file, "wb") as file:
-                file.write(response.content)
-            self.adress = get_nearest_object(self.coordinates, kind="house")
             self.postal_code_click()
-            pixmap = QPixmap(map_file)
-            self.photo_label.setPixmap(pixmap)
-
         except Exception:
-            error_window('Неправильный формат данных')
-            self.reset()
+            self.adress_label.setText('Не удалось загрузить полный адрес :(')
+        pixmap = QPixmap(map_file)
+        self.photo_label.setPixmap(pixmap)
+
+    def mark_from_click(self, mark_ll):
+        self.point_param = f"pt={mark_ll[0]}"
+        self.point_param += f",{mark_ll[1]},pm2blm"
+        self.map_request = f"http://static-maps.yandex.ru/1.x/?{self.ll_spn}&l={self.map_type[-1]}&&size=650,450"
+        self.map_request += "&" + self.point_param
+        print(self.map_request)
+        response = requests.get(self.map_request)
+        map_file = 'map.png'
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        self.adress = get_nearest_object([mark_ll[0], mark_ll[1]], kind="house")
+        if self.adress:
+            self.postal_code_click()
+        pixmap = QPixmap(map_file)
+        self.photo_label.setPixmap(pixmap)
 
     def postal_code_click(self):
-        if self.mark_on:
-            if self.postal_code_button.isChecked():
+        if self.postal_code_button.isChecked():
+            if self.adress['postal_code']:
                 self.adress_label.setText(self.adress["formatted"] + ", " + self.adress["postal_code"])
-            else:
-                self.adress_label.setText(self.adress["formatted"])
-
-    def checked(self):
-        if self.map_type[-1] == "gibrid_on":
-            self.gibrid_on()
-        elif self.map_type[-1] == "shema_on":
-            self.shema_on()
         else:
-            self.sputnik_on()
+            self.adress_label.setText(self.adress["formatted"])
 
     def keyPressEvent(self, event):
         key = event.key()
         if key == Qt.Key_PageUp:
             self.spn[0] = self.spn[0] / 2
             self.spn[1] = self.spn[1] / 2
-            self.checked()
+            self.ll_spn = f"ll={self.coordinates[0]}"
+            self.ll_spn += f",{self.coordinates[1]}"
+            self.ll_spn += f"&spn={self.spn[0]}"
+            self.ll_spn += f",{self.spn[1]}"
+            self.map_image(f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l={self.map_type[-1]}&size=650,450&{self.point_param}")
         elif key == Qt.Key_PageDown:
             self.spn[0] = self.spn[0] * 2
             self.spn[1] = self.spn[1] * 2
-            self.checked()
+            self.ll_spn = f"ll={self.coordinates[0]}"
+            self.ll_spn += f",{self.coordinates[1]}"
+            self.ll_spn += f"&spn={self.spn[0]}"
+            self.ll_spn += f",{self.spn[1]}"
+            self.map_image(
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l={self.map_type[-1]}&size=650,450&{self.point_param}")
         elif key == Qt.Key_Left:
             self.coordinates[0] -= self.spn[0] + self.spn[1]
-            self.checked()
+            self.ll_spn = f"ll={self.coordinates[0]}"
+            self.ll_spn += f",{self.coordinates[1]}"
+            self.ll_spn += f"&spn={self.spn[0]}"
+            self.ll_spn += f",{self.spn[1]}"
+            self.map_image(
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l={self.map_type[-1]}&size=650,450&{self.point_param}")
         elif key == Qt.Key_Right:
             self.coordinates[0] += self.spn[0] + self.spn[1]
-            self.checked()
+            self.ll_spn = f"ll={self.coordinates[0]}"
+            self.ll_spn += f",{self.coordinates[1]}"
+            self.ll_spn += f"&spn={self.spn[0]}"
+            self.ll_spn += f",{self.spn[1]}"
+            self.map_image(
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l={self.map_type[-1]}&size=650,450&{self.point_param}")
         elif key == Qt.Key_Up:
             self.coordinates[1] += self.spn[0] + self.spn[1]
-            self.checked()
+            self.ll_spn = f"ll={self.coordinates[0]}"
+            self.ll_spn += f",{self.coordinates[1]}"
+            self.ll_spn += f"&spn={self.spn[0]}"
+            self.ll_spn += f",{self.spn[1]}"
+            self.map_image(
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l={self.map_type[-1]}&size=650,450&{self.point_param}")
         elif key == Qt.Key_Down:
             self.coordinates[1] -= self.spn[0] + self.spn[1]
-            self.checked()
+            self.ll_spn = f"ll={self.coordinates[0]}"
+            self.ll_spn += f",{self.coordinates[1]}"
+            self.ll_spn += f"&spn={self.spn[0]}"
+            self.ll_spn += f",{self.spn[1]}"
+            self.map_image(
+                f"https://static-maps.yandex.ru/1.x/?ll={f'{self.coordinates[0]},{self.coordinates[1]}'}&spn={f'{self.spn[0]},{self.spn[1]}'}&l={self.map_type[-1]}&size=650,450&{self.point_param}")
+
+    def mousePressEvent(self, event):
+        x = event.x()
+        y = event.y()
+        if 500 <= x <= 1150 and 110 <= y <= 560:
+            x -= 500
+            y -= 110
+            new_x = (x - 325) * self.spn[0] * 1.5 / 325
+            new_y = (y - 225) * self.spn[0] * 0.58 / 225
+            new_pt_ll = [self.coordinates[0] + new_x, self.coordinates[1] - new_y]
+            self.mark_from_click(new_pt_ll)
 
     def reset(self):
         self.mark_on = False
         self.adress_label.setText("")
-        self.checked()
+        self.point_param = ''
+
 
 
 def error_window(text):
